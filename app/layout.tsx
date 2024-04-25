523b71d2f83feb80241f57c198fd2b986c04715a
SymphonyIceAttack <1994370030@qq.com>
2024-04-25 22:38:33 +0800

    feat: use dnd-kit for sort drag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import DndContextProvider from "./ContextProvider/DndContextProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<DndContextProvider>{children}</DndContextProvider>
+			</body>
 		</html>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
-
+import { SortableContext } from "@dnd-kit/sortable";
 import Navigation from "./components/Navigation";
 import WorkSpace from "./components/WorkSpace";
 import { useNavigationList } from "./hooks/use-navigation-list";
 export default function Home() {
 	const [navigationList, setNavigationList] = useNavigationList();
 	return (
-		<main className="h-screen w-full flex">
-			<Navigation navigationList={navigationList} />
-			<WorkSpace />
-		</main>
+		<SortableContext items={navigationList}>
+			<main className="h-screen w-full flex">
+				<Navigation navigationList={navigationList} />
+				<WorkSpace />
+			</main>
+		</SortableContext>
 	);
 }
